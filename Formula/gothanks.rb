fae75b168cc768122344b18d46ecc249931badba
jmowla <71732+jmowla@users.noreply.github.com>
2023-08-26 21:40:05 +0000

    gothanks: add 0.3.0 bottle.

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -5,6 +5,12 @@ class Gothanks < Formula
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/jmowla/homebrew-repo/releases/download/gothanks-0.3.0"
+    sha256 cellar: :any_skip_relocation, ventura:      "80409ef1fe5a4b7a481cc9fc5875a95ba375f787c11aa65fc9fa8a4c9d29bd67"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4dfaeb46f30abdb29b6ee6a29ac56dd1c51cf0cc24fc84ecd9ef32107a62c0cb"
+  end
+
   depends_on "go" => :build
 
   def install
