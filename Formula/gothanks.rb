65bbf61bf3493dea3968aadef4e7998a03bd0baf
Javad Mowla <javad.mowla@sony.com>
2023-08-26 14:27:30 -0700

    Add binary repo
    Closes #2.
    
    Signed-off-by: jmowla <71732+jmowla@users.noreply.github.com>

---
+++ Formula/gothanks.rb
@@ -0,0 +1,17 @@
+class Gothanks < Formula
+  desc "Automatically star your go.mod Github dependencies"
+  homepage "https://github.com/psampaz/gothanks"
+  url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
+  sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "true"
+  end
+end
